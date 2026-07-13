f190cc37b4ab3f6ad466ad43964a63088424c627
kailynholty <kailyn.m.holty.29@dartmouth.edu>
2026-07-13 10:20:40 -0700

    Update week2.py

--- week2.py
+++ week2.py
@@ -48,9 +48,43 @@ listings_columns_drop = listings_missing[listings_missing["Missing Percent"] > 9
 listings_cleaned = listings.drop(columns = listings_columns_drop)
 listings_cleaned.to_csv("listings_cleaned.csv", index = False)
 
+#Listings Columns Dropped:
+#                              Missing Count  Missing Percent  flag_90percent
+#FireplacesTotal                      572122           100.00            True
+#TaxYear                              572122           100.00            True
+#TaxAnnualAmount                      572122           100.00            True
+#CoveredSpaces                        572122           100.00            True
+#BusinessType                         572122           100.00            True
+#MiddleOrJuniorSchoolDistrict         572122           100.00            True
+#AboveGradeFinishedArea               572122           100.00            True
+#ElementarySchoolDistrict             572122           100.00            True
+#BelowGradeFinishedArea               568835            99.43            True
+#CoBuyerAgentFirstName                555696            97.13            True
+#BuilderName                          545587            95.36            True
+#LotSizeDimensions                    542223            94.77            True
+#BuildingAreaTotal                    521643            91.18            True
+
 sold_columns_drop = sold_missing[sold_missing["Missing Percent"] > 90].index
 sold_cleaned = sold.drop(columns = sold_columns_drop)
 sold_cleaned.to_csv("sold_cleaned.csv", index = False)
+
+#Sold Columns Dropped:
+#                              Missing Count  Missing Percent  flag_90percent
+#BusinessType                         491086           100.00            True
+#CoveredSpaces                        491086           100.00            True
+#TaxAnnualAmount                      491086           100.00            True
+#TaxYear                              491086           100.00            True
+#ElementarySchoolDistrict             491086           100.00            True
+#FireplacesTotal                      491086           100.00            True
+#AboveGradeFinishedArea               491086           100.00            True
+#MiddleOrJuniorSchoolDistrict         491086           100.00            True
+#WaterfrontYN                         490773            99.94            True
+#BelowGradeFinishedArea               488248            99.42            True
+#BasementYN                           481524            98.05            True
+#LotSizeDimensions                    467385            95.17            True
+#BuilderName                          466864            95.07            True
+#BuildingAreaTotal                    456773            93.01            True
+#CoBuyerAgentFirstName                446159            90.85            True
 
 #review numeric fields for statistical relevance
 numeric_fields = ["ClosePrice", "ListPrice", "OriginalListPrice", "LivingArea", "LotSizeAcres",
@@ -71,3 +105,8 @@ for field in numeric_fields:
 
 for field in numeric_fields:
     numeric_summary(listings, field)
+
+#determines counties with the highest medians
+county_medians = sold.groupby("CountyOrParish")["ClosePrice"].median()
+county_medians = county_medians.sort_values(ascending = False)
+print(county_medians.head(5))
